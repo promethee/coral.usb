34724eb26d2d59d9fc3a7f47a622bcf18122f9cb
promethee <lunette.francois@gmail.com>
2021-11-08 12:38:15 +0100

    Update run.docker.sh

--- run.docker.sh
+++ run.docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d --name $(basename "$PWD") $USER/$(basename "$PWD"):$(arch)
+docker run -d --privileged -v /dev/bus/usb:/dev/bus/usb --name $(basename "$PWD") $USER/$(basename "$PWD"):$(arch)
